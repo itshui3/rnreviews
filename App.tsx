00f76d07b3fa0159fc5fa7bbe401199503446bb4
Hui <xuhuizhu369@gmail.com>
2021-05-30 20:59:14 -0700

    injects nunitoSans styling

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 import Home from './screens/Home';
 
-export default function App() {
-  return (
-    <Home />
-  );
-}
+const getFonts = () => Font.loadAsync({
+  'nunitoSans-regular': require('./assets/fonts/NunitoSans-Regular.ttf'),
+  'nunitoSans-bold': require('./assets/fonts/NunitoSans-Bold.ttf')
+});
 
+
+export default function App() {
+
+  const [loadedFonts, setLoadedFonts] = useState(false);
+  if (loadedFonts) {
+    return (
+      <Home />
+    )
+  } else {
+    return (
+      <AppLoading 
+      startAsync={getFonts}
+      onFinish={() => setLoadedFonts(true)}
+      onError={console.warn}
+      />
+    )
+  }
+  
+  
+}
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -4,13 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Home() {
     return (
         <View style={styles.container}>
-            <Text>Home Screen</Text>
+            <Text style={styles.titleText}>Home Screen</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        padding: 24
+        padding: 24,
+        
+    },
+    titleText: {
+        fontFamily: 'nunitoSans-bold',
+        fontSize: 18
     }
 });
