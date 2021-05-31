5e4edfa67754b4ff5145584851beed0a2d77a441
Hui <xuhuizhu369@gmail.com>
2021-05-30 20:35:29 -0700

    home/about/details boilerplate

---
+++ screens/About.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function About() {
+    return (
+        <View>
+            <Text>About Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24
+    }
+});
