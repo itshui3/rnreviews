dba741c7948476f7014ba1c0b40ffd01d467c5ac
Hui <xuhuizhu369@gmail.com>
2021-05-30 21:03:07 -0700

    implements and imports global styling

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { globalStyles } from '../styles/global';
+
 export default function Home() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.titleText}>Home Screen</Text>
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.titleText}>Home Screen</Text>
         </View>
     )
 }
