86bebe153897d3d80d432cc3c0e5d78f65670dfd
Hui <xuhuizhu369@gmail.com>
2021-06-02 02:08:17 -0700

    configures header options to indicate which page navigation is on

--- routes/HomeStack.tsx
+++ routes/HomeStack.tsx
@@ -11,8 +11,18 @@ export default function HomeStack(): JSX.Element {
 
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Details" component={Details} />
+            <Stack.Screen 
+            name="Home" component={Home} 
+            options={{ 
+                title: 'List of Reviews'
+            }}
+            />
+            <Stack.Screen 
+            name="Details" component={Details} 
+            options={({ route }) => ({ 
+                title: `${(route.params as any).key}: ${(route.params as any).title}`
+            })}
+            />
         </ Stack.Navigator>
     )
 }
